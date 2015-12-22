7ae38d0a25283381b044e93fe337cf6b127ff2c4
Michael Schmitz <Michael.Schmitz@T-Systems.com>
2015-12-22 10:32:17 +0100

    Main program for assignment
    Main program

---
+++ run_analysis.r
@@ -0,0 +1,15 @@
+library(sqldf)
+source("programs/getRawData.r")
+setwd("data/UCI HAR Dataset")
+dfTrain <- getRawData("train")
+dfTest <- getRawData("test")
+dfAll <- rbind(dfTrain, dfTest)
+
+# Summarize
+dfBy <- dfAll[, 1:2]
+dfNum <- dfAll[, 3:81]
+tidy <- aggregate(x = dfNum, by = list(dfBy$Subject, dfBy$Activity), FUN = mean)
+colnames(tidy)[1:2] <- colnames(dfAll)[1:2]
+print(dim(tidy))
+print(head(tidy, 2))
+write.table(x = tidy, file = "output/tidy.txt", row.name = FALSE)
