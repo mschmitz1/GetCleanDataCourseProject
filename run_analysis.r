7254b9b7b805601b0a80afc9da0f0d226e2394fc
Michael Schmitz <Michael.Schmitz@T-Systems.com>
2015-12-26 18:17:36 +0100

    Korrektur und Ergänzung

--- run_analysis.r
+++ run_analysis.r
@@ -12,4 +12,7 @@ tidy <- aggregate(x = dfNum, by = list(dfBy$Subject, dfBy$Activity), FUN = mean)
 colnames(tidy)[1:2] <- colnames(dfAll)[1:2]
 print(dim(tidy))
 print(head(tidy, 2))
+if (!file.exists("output")) {
+	dir.create("output")
+}
 write.table(x = tidy, file = "output/tidy.txt", row.name = FALSE)
